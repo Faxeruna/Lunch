fa26becddda1b101422ac42f8e46a2688b5e187e
faxeruna73 <faxeruna73@gmail.com>
2019-06-25 20:35:32 +0400

    agor

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,30 +1,47 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 
 export default class Navigation extends Component {
-  render(){
-    const { location } = this.props;
+  render() {
+    const {location} = this.props;
     return (
-      <Navbar bg="primary">
+      <div>
         <Container>
-          <Navbar.Brand>
-            <Link to="/" className="navbar-brand">Lunch</Link>
-          </Navbar.Brand>
-          <Nav activeKey={location}>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/users">Users</Nav.Link>
-            <Nav.Link href="/registration">Registration</Nav.Link>
-            <Nav.Link href="/users/newOrder">New order</Nav.Link>
-            <Nav.Link href="/users/orderList">Order list</Nav.Link>
-            <Nav.Link href="/admin/newOrderAdmin">New order admin</Nav.Link>
-            <Nav.Link href="/admin/orderListAdmin">Order list admin</Nav.Link>
-            <Nav.Link href="/admin/reportAdmin">Report admin</Nav.Link>
-          </Nav>
+          <Navbar bg="primary">
+            <Navbar.Brand>
+              <Link to="/" className="navbar-brand">Lunch</Link>
+            </Navbar.Brand>
+            
+            <Nav flex="column sm-row" activeKey={location}>
+              <Nav.Item>
+                <Dropdown>
+                  <Dropdown.Toggle drop='left' id="dropdown-basic">
+                    Menu
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="/users">Profile page</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="/users/newOrder">New order</Dropdown.Item>
+                    <Dropdown.Item href="/users/orderList">Order list</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="/admin/newOrderAdmin">New order(admin)</Dropdown.Item>
+                    <Dropdown.Item href="/admin/orderListAdmin">Order list(admin)</Dropdown.Item>
+                    <Dropdown.Item href="/admin/reportAdmin">Report(admin)</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="/">Login</Dropdown.Item>
+                    <Dropdown.Item href="/registration">Registration</Dropdown.Item>
+                    <Dropdown.Item href="/">Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Nav.Item>
+            </Nav>
+          </Navbar>
         </Container>
-      </Navbar>
+      </div>
     );
   }
 }
--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default class ContactPage extends Component{
   render(){
     return (
     <Container>
-      <h1>Login</h1>
-      <p>
-        Login page.
-      </p>
+      <Row>
+        <Col md={4} sm={4}>
+        </Col>
+        <Col md='auto' sm={4}>
+          <form className="form-signin">
+            <h2 className="form-signin-heading">Please sign in</h2>
+            <label for="inputEmail" className="sr-only">Email address</label>
+            <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+            <label for="inputPassword" className="sr-only">Password</label>
+            <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+            <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button">Sign in</button>
+          </form>
+          <Link to="/registration">{'Registration'}</Link>
+        </Col>
+        <Col md={4} sm={4}>
+        </Col>
+      </Row>
     </Container>
     )
   }
